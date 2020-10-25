c05f141401aeae24855f22b37accceb6b5b96139
Ben Dechawonganun <benjarat.de@gmail.com>
2020-10-25 13:51:19 +0700

    initial commmit

---
+++ TestGitHub/src/th/co/ben/Object/personal.java
@@ -0,0 +1,32 @@
+package th.co.ben.Object;
+
+public class personal {
+	private String name;
+	private String lastName;
+	private String idCard;
+	private String mobileNo;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getIdCard() {
+		return idCard;
+	}
+	public void setIdCard(String idCard) {
+		this.idCard = idCard;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+}
---
+++ TestGitHub/src/th/co/ben/Processing/mainOne.java
@@ -0,0 +1,10 @@
+package th.co.ben.Processing;
+
+public class mainOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
