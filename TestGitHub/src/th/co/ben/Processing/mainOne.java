8e135a81576413a196d3b4c304cd7ec4254915bc
Ben Dechawonganun <benjarat.de@gmail.com>
2020-10-25 16:14:15 +0700

    gitignore

--- TestGitHub/src/th/co/ben/Processing/mainOne.java
+++ TestGitHub/src/th/co/ben/Processing/mainOne.java
@@ -1,7 +1,10 @@
 package th.co.ben.Processing;
 
+import th.co.ben.Object.personal;
+
 public class mainOne {
 
+	personal p = new personal();
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
